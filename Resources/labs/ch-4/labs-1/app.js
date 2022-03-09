02d17a3716dc8d9b692abbdc339f2fac6df4cab5
ylehilds <lehi_alcantara@byu.edu>
2022-03-08 23:03:23 -0700

    added terminal commands to chapter 4

--- Resources/labs/ch-4/labs-1/app.js
+++ Resources/labs/ch-4/labs-1/app.js
@@ -3,3 +3,8 @@ function f (n = 99) {
   f(n - 1)
 }
 f()
+
+// terminal command: node --inspect-brk app.js
+// then on the browser go to: chrome://inspect, then find the
+// process under "Remote Targets", clicking "inspect" and ensuring the "Sources" tab is selected
+
--- Resources/labs/ch-4/labs-2/app.js
+++ Resources/labs/ch-4/labs-2/app.js
@@ -4,3 +4,7 @@ function f (n = 99) {
   f(n - 1)
 }
 f()
+
+// terminal command: node --inspect app.js a note on this is that this is the right command to make it stop on debugger keyword, but I can't make it to stop and therefore I use the flag --inspect-brk then run to nect break point which is the debugger keyword.
+// then on the browser go to: chrome://inspect, then find the
+// process under "Remote Targets", clicking "inspect" and ensuring the "Sources" tab is selected
