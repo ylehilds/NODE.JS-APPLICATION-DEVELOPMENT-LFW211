6595252d667cf84485d5165123523433d1446d9a
ylehilds <lehi_alcantara@byu.edu>
2022-03-13 18:40:11 -0600

    modified to be one liner.

--- Resources/labs/ch-11/labs-2/index.js
+++ Resources/labs/ch-11/labs-2/index.js
@@ -1,8 +1,8 @@
 'use strict'
 const assert = require('assert')
 const str = 'buffers are neat'
-const buffer = Buffer.from(str)
-const base64 =  buffer.toString('base64') // convert str to base64
+// const buffer = Buffer.from(str)
+const base64 =  Buffer.from(str).toString('base64') // converts string to buffer then converts buffer to string base64
 
 console.log(base64)
 
