d088c4ed251aaa2bceab4ffe26cfd9af1a21a058
ylehilds <lehi_alcantara@byu.edu>
2022-03-21 22:50:19 -0600

    updated tests implementation.

--- Resources/labs/ch-16/labs-1/test/uppercase.test.js
+++ Resources/labs/ch-16/labs-1/test/uppercase.test.js
@@ -1,11 +1,9 @@
 'use strict'
 const uppercase = require('../uppercase')
 test('throw when output is not uppercase', async () => {
-  expect(uppercase('lehi')).toStrictEqual('LEHI')
+  expect(uppercase('lehi')).toBe('LEHI')
 })
 
 test('throw when inputs is not string', async () => {
-  expect(() => uppercase(5)).toThrowError(
-    Error('input must be a string')
-  )
+  expect(() => uppercase(5)).toThrow('input must be a string')
 })
