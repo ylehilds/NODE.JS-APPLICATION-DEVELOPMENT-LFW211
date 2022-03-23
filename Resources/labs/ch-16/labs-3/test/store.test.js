b1a7838b7e7e0f538150cd992c005c17138d3091
ylehilds <lehi_alcantara@byu.edu>
2022-03-22 23:26:19 -0600

    refactored implementation

--- Resources/labs/ch-16/labs-3/test/store.test.js
+++ Resources/labs/ch-16/labs-3/test/store.test.js
@@ -1,15 +1,24 @@
 const store = require('../store')
 
-test('input must be a buffer', async () => {
-  await expect(store('testingThis'))
+test('input needs to be a buffer', async () => {
+  const storeCode = 'test'
+  expect( async () => {
+    expect(await store(storeCode)).toThrow('input must be a buffer')
+  })
+})
+
+test('input rejection', async () => {
+  const storeCode = 'test'
+  await expect(store(storeCode))
     .rejects
     .toThrow('input must be a buffer')
 })
 
-test('responds with data id length of 4', async () => {
-  const value = Buffer.from('2775736520737472696374270a6d6f64756c652e6578706f727473203d202876616c75652c20636229203d3e207b0a2020696620284275666665722e69734275666665722876616c756529203d3d3d2066616c736529207b0a202020206362284572726f722827696e707574206d7573742062652061206275666665722729290a2020202072657475726e0a20207d0a202073657454696d656f7574282829203d3e207b0a20202020636f6e7374206964203d204d6174682e72616e646f6d28292e746f537472696e67283336292e73706c697428272e27295b315d2e736c69636528302c2034290a202020206362286e756c6c2c207b206964207d290a20207d2c20333030290a7d0a', 'hex')
-  const data = await store(value)
-  expect(Buffer.isBuffer(value)).toBeTruthy()
-  expect(data.id.length).toBe(4)
+test('data id needs to be 4 in length and input is a buffer', async () => {
+  const storeId = {id: 1234}
+  const storeCode = Buffer.from(storeId.toString())
+  expect(Buffer.isBuffer(storeCode)).toBeTruthy()
+  const myStore = await store(storeCode)
+  expect(myStore.id.length).toBe(4)
+  expect(myStore.id.length).not.toBe(5)
 })
-
