8600e0d35d837a7c88cf4b51e1502619e3752216
ylehilds <lehi_alcantara@byu.edu>
2022-05-05 08:44:11 -0600

    improved buffer task

--- preparation/Buffer/buffer.js
+++ preparation/Buffer/buffer.js
@@ -1,4 +1,10 @@
 // Task: Create a string buffer that is 256 bytes long
 
-const buffer = Buffer.alloc(256)
-console.log(buffer)
+const buf = Buffer.alloc(256);
+const len = buf.write("Simply Easy Learning");
+const json = buf.toJSON(buf);
+
+console.log("Octets written : "+  len);
+console.log("buffer content string: "+  buf.toString());
+console.log("buffer content base64: "+  buf.toString('base64'));
+console.log(json);
